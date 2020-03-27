849e203ee527e3ebdddafa8830fbb67f05e853c2
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-03-27 19:21:09 +0100

    Fix typos and formats in DB access component

--- main-app/client/src/components/DBAccessTester/index.js
+++ main-app/client/src/components/DBAccessTester/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import EntityTest from "./EntityTest";
 import UserCreateForm from "./EntityCreateForms/User";
 import DocumentCreateForm from "./EntityCreateForms/Document";
@@ -6,31 +6,46 @@ import ConfigCreateForm from "./EntityCreateForms/Config";
 import "./style.css";
 
 class DBAccessTester extends Component {
-    render() {
-        const userCreateForm = (
-            <UserCreateForm endpointResourcePath={`${this.props.baseEndpointResourcePath}/users`}/>
-        );
-        const documentCreateForm = (
-            <DocumentCreateForm endpointResourcePath={`${this.props.baseEndpointResourcePath}/documents`}/>
-        );
-        const configCreateForm = (
-            <ConfigCreateForm endpointResourcePath={`${this.props.baseEndpointResourcePath}/configs`}/>
-        );
+  render() {
+    const userCreateForm = (
+      <UserCreateForm
+        endpointResourcePath={`${this.props.baseEndpointResourcePath}/users`}
+      />
+    );
+    const documentCreateForm = (
+      <DocumentCreateForm
+        endpointResourcePath={`${this.props.baseEndpointResourcePath}/documents`}
+      />
+    );
+    const configCreateForm = (
+      <ConfigCreateForm
+        endpointResourcePath={`${this.props.baseEndpointResourcePath}/configs`}
+      />
+    );
 
-        return (
-            <div className="db-entities-container">
-                <EntityTest title="Users" createEntityText="Create documents"
-                            entityResourcePath={`${this.props.baseEndpointResourcePath}/users`}
-                            createFormComponent={userCreateForm}/>
-                <EntityTest title="Documents" createEntityText="Create documents"
-                            entityResourcePath={`${this.props.baseEndpointResourcePath}/documents`}
-                            createFormComponent={documentCreateForm}/>
-                <EntityTest title="Configs" createEntityText="Create configs"
-                            entityResourcePath={`${this.props.baseEndpointResourcePath}/configs`}
-                            createFormComponent={configCreateForm}/>
-            </div>
-        );
-    }
+    return (
+      <div className="db-entities-container">
+        <EntityTest
+          title="Users"
+          createEntityText="Create user"
+          entityResourcePath={`${this.props.baseEndpointResourcePath}/users`}
+          createFormComponent={userCreateForm}
+        />
+        <EntityTest
+          title="Documents"
+          createEntityText="Create document"
+          entityResourcePath={`${this.props.baseEndpointResourcePath}/documents`}
+          createFormComponent={documentCreateForm}
+        />
+        <EntityTest
+          title="Configs"
+          createEntityText="Create config"
+          entityResourcePath={`${this.props.baseEndpointResourcePath}/configs`}
+          createFormComponent={configCreateForm}
+        />
+      </div>
+    );
+  }
 }
 
 export default DBAccessTester;
